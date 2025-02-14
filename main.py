04f43bcbd57d3e4e40a649bb8ee94da1fdcef7d8
zilmarij <zilmarij1229@gmail.com>
2025-02-14 14:29:32 +0530

    extract text from HTML tags

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ soup = BeautifulSoup(page.content, "html.parser")
 # print(soup.prettify())
 
 #page parse
-pTag= soup.p
+
+# print(soup.find_all('p'))
+# print(soup.find_all('p')[0].get_text())
+
+object = soup.find(id="mp-left")
+items = object.find_all(class_="mp-h2")
+result = items[0]
+# print(result.prettify())
+
+# print(soup.script.string)
+# print(soup.find('script').string)
+print(soup.find_all('script')[0].string)
+
+# pTag= soup.p
 # print(pTag.string)
 # print(list(soup.children))
 
-# print(soup.find_all('p'))
-
-# return only text
-# does not include HTML tags
-print(soup.find_all('p')[0].get_text())
-
-# print(soup.find_all(class_="mp-h2")[0])
+# f = soup.find(id='printfooter')   
+# print(f)
